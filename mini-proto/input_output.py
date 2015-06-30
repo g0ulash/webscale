b91e74b1e7e36eec4657911288331cfc9096c837
Niklas Weber <nalderan@gmail.com>
2015-06-30 02:04:22 +0200

    sqlite caching: write contexts

--- mini-proto/input_output.py
+++ mini-proto/input_output.py
@@ -1,10 +1,14 @@
+import copy
 import json
+import os
 import urllib2
+import abc
+from abc import abstractmethod
+import sqlite3
+
 
 __author__ = 'niklas'
 
-import abc
-from abc import abstractmethod
 
 
 class AbstractCache():
@@ -37,7 +41,6 @@ class AbstractCache():
 
 
 class NoneCache(AbstractCache):
-
     def __init__(self):
         pass
 
@@ -60,6 +63,36 @@ class NoneCache(AbstractCache):
         pass
 
 
+class SQLiteCache(AbstractCache):
+    """2015-06-30 00:01:30,461 [MainThread  ] [INFO ]  context received:
+    {u'Referer': u'NA', u'Age': 43.0, u'Language': u'EN', u'ID': 3493, u'Agent': u'OSX'}"""
+
+    def __init__(self):
+        self.conn = sqlite3.connect(os.path.join(".", "cache", "sqlite-cache.db"))
+        self.context_keys = sorted(["Referer", "Age", "Language", "ID", "Agent"])
+
+        columns = "(run_id integer, interaction_id integer, Age integer, Agent text, ID integer, Language text, Referer text)"
+        q = "CREATE TABLE IF NOT EXISTS contexts {}".format(columns)
+        self.conn.execute(q)
+
+    def set_context(self, run_id, interaction_id, context):
+        cc = copy.copy(context)
+        cc["run_id"] = run_id
+        cc["interaction_id"] = interaction_id
+        q = "INSERT INTO contexts VALUES (:run_id,:interaction_id,:Age,:Agent,:ID,:Language,:Referer)"
+        self.conn.execute(q, cc)
+        self.conn.commit()
+
+    def get_context(self, run_id, interaction_id):
+        pass
+
+    def get_click(self, run_id, interaction_id, ad):
+        pass
+
+    def set_user_reaction(self, run_id, interaction_id, reaction):
+        pass
+
+
 class InputOutput():
     """
     Handle interaction with the pseudo-user
@@ -68,7 +101,7 @@ class InputOutput():
     def __init__(self):
         self.teamid = "Smartass1337$$BillYo"
         self.teampw = "12246bef4f7093a8a3d78dff975e180f"
-        self.cache = NoneCache()
+        self.cache = SQLiteCache()
 
     def get_context(self, run_id, interaction_id):
         cached = self.cache.get_context(run_id, interaction_id)
@@ -78,10 +111,10 @@ class InputOutput():
                                                                                                        run_id,
                                                                                                        self.teamid,
                                                                                                        self.teampw)
-        #print("requesting url: " + source)
+        # print("requesting url: " + source)
         response = json.load(urllib2.urlopen(source))
         context = response["context"]
-        #print("received context: {}".format(context))
+        # print("received context: {}".format(context))
         self.cache.set_context(run_id, interaction_id, context)
         return context
 
@@ -99,8 +132,8 @@ class InputOutput():
             ad["productid"],
             ad["price"],
             self.teampw)
-        #print("requesting url for click: {}".format(url))
+        # print("requesting url for click: {}".format(url))
         reaction = json.load(urllib2.urlopen(url))
-        #print("got response: {}".format(response))
+        # print("got response: {}".format(response))
         self.cache.set_user_reaction(run_id, interaction_id, reaction)
         return reaction
